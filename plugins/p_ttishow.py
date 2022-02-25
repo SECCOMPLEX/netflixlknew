e27a39695955cffcad181f377710ea22f5f04869
SECCOMPLEX <95067541+SECCOMPLEX@users.noreply.github.com>
2022-02-25 21:18:32 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('Main Channel', url='https://t.me/infinityLK')
+            InlineKeyboardButton('Main Channel', url='https://t.me/SECL4U')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
